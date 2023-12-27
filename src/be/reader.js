22d0fb189479ba13f5676b0c665ffc2723003706
wesley <9610848@qq.com>
2023-12-27 09:07:14 +0800

    prevpage fix

--- src/be/reader.js
+++ src/be/reader.js
@@ -1,6 +1,38 @@
 function plugin() {
+  var isScrolling,
+    isAtBottom = null,
+    isAtTop = null;
+  document.addEventListener("wheel", function (event) {
+    clearTimeout(isScrolling);
+    if (isAtTop == null) {
+      var scrollY = window.scrollY;
+      var tolerance = 2;
+      isAtTop = scrollY - 2 <= 0;
+    }
+    if (isAtBottom == null) {
+      var documentHeight = document.documentElement.scrollHeight;
+      var windowHeight =
+        window.innerHeight || document.documentElement.clientHeight;
+      var scrollY = window.scrollY;
+      var tolerance = 2;
+      isAtBottom = scrollY + windowHeight + tolerance >= documentHeight;
+    }
+    isScrolling = setTimeout(function () {
+      if (event.deltaY < 0) {
+        console.log("双指上划");
+        if (isAtTop) {
+          window.parent.postMessage("prevpage", "*");
+        }
+      } else {
+        if (isAtBottom) {
+          window.parent.postMessage("nextpage", "*");
+        }
+      }
+      isAtBottom = null;
+      isAtTop = null;
+    }, 200);
+  });
   document.addEventListener("keydown", (e) => {
-    console.log("key pressed", e);
     if (e.key == "ArrowRight") {
       window.parent.postMessage("nextpage", "*");
     } else if (e.key == "ArrowLeft") {
